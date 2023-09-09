b827ece37bc63326004c85a927a49590b6ca356f
Avinash Chaurasiya <89199468+avinashk2000@users.noreply.github.com>
2023-09-09 23:54:15 +0530

    Add files via upload

---
+++ database/portfolio.sql
@@ -0,0 +1,235 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 09, 2023 at 06:14 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_log`
+--
+
+DROP TABLE IF EXISTS `admin_log`;
+CREATE TABLE IF NOT EXISTS `admin_log` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin_log`
+--
+
+INSERT INTO `admin_log` (`id`, `name`, `user_name`, `password`, `create_at`) VALUES
+(1, 'Avinash Kumar', 'avinash8564kumar@gmail.com', 'avinashfolio@85', '2023-09-09 06:08:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_info`
+--
+
+DROP TABLE IF EXISTS `contact_info`;
+CREATE TABLE IF NOT EXISTS `contact_info` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `contact_info`
+--
+
+INSERT INTO `contact_info` (`id`, `name`, `email`, `subject`, `message`, `create_at`) VALUES
+(1, 'shobhit Kumar Jha', 'avinashk8650@gmail.com', 'Shikayat', 'Hello Avinash', '2023-09-09 17:08:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experience`
+--
+
+DROP TABLE IF EXISTS `experience`;
+CREATE TABLE IF NOT EXISTS `experience` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `experience` varchar(100) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `details` text NOT NULL,
+  `start_time` date NOT NULL,
+  `leave_time` date NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hire_details`
+--
+
+DROP TABLE IF EXISTS `hire_details`;
+CREATE TABLE IF NOT EXISTS `hire_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `technology` varchar(255) NOT NULL,
+  `contact_number` bigint NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `job_type` varchar(50) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `my_projects`
+--
+
+DROP TABLE IF EXISTS `my_projects`;
+CREATE TABLE IF NOT EXISTS `my_projects` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(255) NOT NULL,
+  `project_link` varchar(255) NOT NULL,
+  `project_image` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `my_projects`
+--
+
+INSERT INTO `my_projects` (`id`, `project_name`, `project_link`, `project_image`, `created_at`) VALUES
+(1, 'Hotel Management System', 'https://github.com/avinashk2000/online-hotel-management/tree/master', 'Hotel Management System781178354.jpg', '2023-09-09 17:48:54'),
+(2, 'Calculator Application', 'https://github.com/avinashk2000/Kotlin-Calculator', 'Calculator Application1996420831.jpg', '2023-09-09 17:57:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role` varchar(250) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `role` (`role`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `role`, `create_at`) VALUES
+(1, 'Web Designer', '2023-09-09 15:54:07'),
+(2, 'Web Developer', '2023-09-09 15:54:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+DROP TABLE IF EXISTS `service`;
+CREATE TABLE IF NOT EXISTS `service` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `service` varchar(100) NOT NULL,
+  `icon` varchar(100) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `service` (`service`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`id`, `service`, `icon`, `create_at`) VALUES
+(1, 'Web Development', 'Web Development1082021280.png', '2023-09-09 15:52:22'),
+(2, 'Website Design', 'Website Design472324056.png', '2023-09-09 15:53:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skill`
+--
+
+DROP TABLE IF EXISTS `skill`;
+CREATE TABLE IF NOT EXISTS `skill` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `skill_name` varchar(255) NOT NULL,
+  `percentage` int NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `skill`
+--
+
+INSERT INTO `skill` (`id`, `skill_name`, `percentage`, `create_at`) VALUES
+(1, 'HTML', 100, '2023-09-09 15:58:04'),
+(2, 'CSS', 95, '2023-09-09 15:58:13'),
+(3, 'JavaScript', 90, '2023-09-09 15:59:48'),
+(4, 'Bootstrap', 100, '2023-09-09 16:00:12'),
+(5, 'React.Js', 40, '2023-09-09 16:00:26'),
+(6, 'jQuery', 80, '2023-09-09 16:00:41'),
+(7, 'MySQL', 90, '2023-09-09 16:00:55'),
+(8, 'PHP', 95, '2023-09-09 16:01:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `technology`
+--
+
+DROP TABLE IF EXISTS `technology`;
+CREATE TABLE IF NOT EXISTS `technology` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `technology` varchar(255) NOT NULL,
+  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `technology`
+--
+
+INSERT INTO `technology` (`id`, `technology`, `create_at`) VALUES
+(1, 'PHP', '2023-09-09 16:08:13'),
+(2, 'JavaScript', '2023-09-09 16:08:42'),
+(3, 'React.Js', '2023-09-09 16:09:37');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
